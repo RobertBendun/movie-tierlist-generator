16dd5a1801f4ec185b3604fa3031cadf3191a2e6
Robert Bendun <rob.bendun@gmail.com>
2022-12-25 04:58:44 +0100

    cleanup; fix layout bug for more then 1 row content

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xe -o pipefail
+set -e -o pipefail
 
 if [ -z "$YEAR" ]; then
 	export YEAR="$(date +%Y)"
@@ -8,9 +8,23 @@ else
 	export YEAR="$YEAR"
 fi
 
-npx ts-node ids.ts > ids
+. .env
 
-./request.sh
+Base_Url="http://www.omdbapi.com/?apikey=$OMDB_KEY&i="
+
+mkdir -p posters
+cd posters
+for id in $(cd ..; npx ts-node ids.ts); do
+	if [ -f "$id.jpg" ]; then
+		echo "Skipping ${id}"
+	else
+		echo "Fetching ${id}"
+		url="$(curl -s "${Base_Url}${id}" | gron |  awk -F'"' '/\.Poster/ { print $2 }')"
+		wget "$url" --quiet -O "$id.jpg"
+	fi
+done
+cd ..
+
 
 cat header.html <(npx ts-node groups.ts) footer.html > index.html
 sed -i "s/__YEAR__/$YEAR/" index.html
